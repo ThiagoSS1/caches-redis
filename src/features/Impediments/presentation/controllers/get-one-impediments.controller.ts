c116d192b9ae48316eb7ea088d4a78e8ed8021d1
ThiagoRaposo <thiago@ciclix.com>
2022-01-26 19:21:44 -0300

    added cache get-one-impediments

--- src/features/Impediments/presentation/controllers/get-one-impediments.controller.ts
+++ src/features/Impediments/presentation/controllers/get-one-impediments.controller.ts
@@ -3,28 +3,38 @@ import { Controller } from "../../../../core/presentation/contracts/controller";
 import { ImpedimentRepository } from "../../infra/repositories/impediments.repository";
 import { Impediments } from "../../domain/models/impediments";
 import { CacheRepository } from "../../../../core/infra/repositories/cache.repository";
-import { notFound, ok, serverError } from "../../../../core/presentation/helpers/http-helper";
+import {
+  notFound,
+  ok,
+  serverError,
+} from "../../../../core/presentation/helpers/http-helper";
 export class GetOneImpedimentsController implements Controller {
-    async handle(req: Request, res: Response): Promise<any> {
-        try {
-            const { uid } = req.params;
+  async handle(req: Request, res: Response): Promise<any> {
+    try {
+      const { uid } = req.params;
 
-            //const cache = new CacheRepository();
+      // cria uma instância do repositorio que cuida do cache
+      const cache = new CacheRepository();
 
-            //const ImpedimentsCache: Impediments = await cache.get(`impediments:${uid}`);
+      // recupera o registro no cache
+      const ImpedimentsCache: Impediments = await cache.get(
+        `impediments:${uid}`
+      );
 
-            // if (ImpedimentsCache) {
-            //     return ok(res, )
-            // }
+      // verifica se encontrou e retorna caso verdadeiro
+      if (ImpedimentsCache) {
+        return ok(res, ImpedimentsCache);
+      }
 
-            const repository = new ImpedimentRepository();
-            const impediment = await repository.getByUid(uid);
+      // se não encontrou no cache é buscado na base dados
+      const repository = new ImpedimentRepository();
+      const impediment = await repository.getByUid(uid);
 
-            if(!impediment) return notFound(res)
+      if (!impediment) return notFound(res);
 
-            return ok(res, impediment)
-        } catch (error: any) {
-            return serverError(res, error)
-        }
+      return ok(res, impediment);
+    } catch (error: any) {
+      return serverError(res, error);
     }
+  }
 }
